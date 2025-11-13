b0dcf430dd413887f05346e12544713e9f169d4e
ZoeKhan1 <zkhan@smith.edu>
2025-11-13 15:34:00 -0500

    Update cleaning function

--- R/clean_epa.R
+++ R/clean_epa.R
@@ -4,6 +4,7 @@
 #' @param data ETA TRI dataset for a state and year
 #' @return a data frame
 #' @importFrom janitor clean_names
+#' @importFrom dplyr select rename_with
 #' @export
 #' @examples
 #' \dontrun{
@@ -18,6 +19,8 @@ clean_epa <- function(data) {
            x9_zip, x12_latitude, x13_longitude, x15_parent_co_name, x21_federal_facility,
            x23_industry_sector, x37_chemical, x38_elemental_metal_included,
            x42_clean_air_act_chemical, x43_classification, x44_metal,
-           x46_carcinogen, x47_pbt, x48_pfas, x50_unit_of_measure, x107_total_releases)
+           x46_carcinogen, x47_pbt, x48_pfas, x50_unit_of_measure, x107_total_releases) |>
+    rename_with(~ substring(., 4), .cols = 1:7) |>
+    rename_with(~ substring(., 5), .cols = 8:22)
   return(cleaned)
 }
--- R/data.R
+++ R/data.R
@@ -2,9 +2,30 @@
 #'
 #' Contains chemical release data from Massachusetts in 2024. Can be used as an example data set for any package functions.
 #'
-#' @format A data frame with 820 rows and 122 variables:
+#' @format A data frame with 820 rows and 22 variables:
 #' \describe{
-#'   \item{test}{test}
+#'   \item{x1_year}{test}
+#'   \item{x3_frs_id}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'   \item{}{}
+#'
 #' }
 #'
 "ma_2024"
