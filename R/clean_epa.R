8b6a11c6a2b82d063a8d35d1f0b0b5518067e150
ZoeKhan1 <zkhan@smith.edu>
2025-11-13 15:22:06 -0500

    Add Texas example data and update cleaning function

--- R/clean_epa.R
+++ R/clean_epa.R
@@ -13,5 +13,11 @@
 
 clean_epa <- function(data) {
   cleaned <- clean_names(data)
+  cleaned <- cleaned |>
+    select(x1_year, x3_frs_id, x4_facility_name, x6_city, x7_county, x8_st,
+           x9_zip, x12_latitude, x13_longitude, x15_parent_co_name, x21_federal_facility,
+           x23_industry_sector, x37_chemical, x38_elemental_metal_included,
+           x42_clean_air_act_chemical, x43_classification, x44_metal,
+           x46_carcinogen, x47_pbt, x48_pfas, x50_unit_of_measure, x107_total_releases)
   return(cleaned)
 }
--- raw_data/writing_example_data.R
+++ raw_data/writing_example_data.R
@@ -2,8 +2,13 @@ library(janitor)
 
 # Read in the EPA TRI example data from the raw-data folder
 ma_2024 <- read.csv("raw_data/2024_ma.csv")
+tx_2024 <- read.csv("raw_data/2024_tx.csv")
 
-ma_2024 <- clean_names(ma_2024)
+
+# Cleaning the data
+ma_2024 <- clean_epa(ma_2024)
+tx_2024 <- clean_epa(tx_2024)
 
 # Write this data into the package. Setting "overwrite = TRUE" is necessary if you need to rewrite the data set.
 usethis::use_data(ma_2024, overwrite = TRUE)
+usethis::use_data(tx_2024, overwrite = TRUE)
