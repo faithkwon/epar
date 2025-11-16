e30209802b8e7ea14e0c0ee482e7d2b017834b7e
ZoeKhan1 <zkhan@smith.edu>
2025-11-16 01:26:48 -0500

    Fix cmd check issues

--- tests/testthat/test-chemstats.R
+++ tests/testthat/test-chemstats.R
@@ -1,12 +1,12 @@
-test_that("chemstats works", {
+testthat::test_that("chemstats works", {
   # test that # of counties is correct
-  expect_equal(nrow(chemstats(ma_2024, "county")), n_distinct(ma_2024[["county"]]))
+  expect_equal(nrow(chemstats(ma_2024, "county")), dplyr::n_distinct(ma_2024[["county"]]))
 
   # test that # of cities is correct
-  expect_equal(nrow(chemstats(ma_2024, "city")), n_distinct(ma_2024[["city"]]))
+  expect_equal(nrow(chemstats(ma_2024, "city")), dplyr::n_distinct(ma_2024[["city"]]))
 
   # test that # of zips is correct
-  expect_equal(nrow(chemstats(ma_2024, "zip")), n_distinct(ma_2024[["zip"]]))
+  expect_equal(nrow(chemstats(ma_2024, "zip")), dplyr::n_distinct(ma_2024[["zip"]]))
 
 
 })
